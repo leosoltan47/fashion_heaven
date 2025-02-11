8b6a9b439503f905fc75344ad3d6508fd9e8ba59
Renter-0 <nadox45@gmail.com>
2025-02-11 18:57:39 +0300

    feat: add template for pages
    HTML, CSS, JS cherry-picked from @leosoltan47
    
    Co-authored-by: leosoltan47 <leosoltan47@users.noreply.github.com>

--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,8 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 # TODO: Add HTML template to render main page
 # TODO: Add function to randomly (for now) select products to showcase on the main page
 # TODO: Add search bar that renders page specified in the search
-def index(request):
-    return HttpResponse(b"You're on the index page hooray")
+def home(request):
+    return render(request, "pages/home.html")
