6a5adeaf6d5b02e0f5fc3292b5cafc656e1860e4
Renter-0 <nadox45@gmail.com>
2025-02-12 19:28:13 +0300

    feat: embed products into featured section

--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from .models import Products
 
 
 # TODO: Add HTML template to render main page
 # TODO: Add function to randomly (for now) select products to showcase on the main page
 # TODO: Add search bar that renders page specified in the search
 def home(request):
-    return render(request, "pages/home.html")
+    products = Products.objects.all()
+    context = {"enumerated_products": enumerate(products)}
+    return render(request, "pages/home.html", context)
