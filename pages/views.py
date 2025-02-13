02c1f36438f7bc0429764cabd9eafa1967a0756f
Renter-0 <nadox45@gmail.com>
2025-02-13 13:43:41 +0300

    feat: fill `home.html` with value from db
    Inserts product names and product prices

--- pages/views.py
+++ pages/views.py
@@ -2,9 +2,8 @@ from django.shortcuts import render
 from .models import Products
 
 
-# TODO: Add HTML template to render main page
 # TODO: Add search bar that renders page specified in the search
 def home(request):
-    products = Products.objects.all()
-    context = {"enumerated_products": enumerate(products)}
+    products = Products.objects.all()[:10]
+    context = {"products": products}
     return render(request, "pages/home.html", context)
