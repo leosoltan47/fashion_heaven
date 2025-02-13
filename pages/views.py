6d1534f6f2f06bdcaaaad94d683f4cd5e45d9906
Renter-0 <nadox45@gmail.com>
2025-02-13 22:09:12 +0300

    feat: fill new templates

--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,12 @@
+# TODO: Add logger and profiler to monitor performance
 from django.shortcuts import render
 from .models import Products
 
 
 # TODO: Add search bar that renders page specified in the search
 def home(request):
-    products = Products.objects.all()[:10]
+    # Home page is designed to show 10 products
+    # FIXME: Severe performance hit due to this query
+    products = Products.objects.prefetch_related("productdetails_set").all()[:10]
     context = {"products": products}
     return render(request, "pages/home.html", context)
