9bc4cce32dcf8fe35ba59f4d36efce562a7ee5a5
Renter-0 <nadox45@gmail.com>
2025-02-13 13:05:56 +0300

    feat: allow admin to access db

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Products)
+admin.site.register(ProductDetails)
+admin.site.register(ProductImages)
+admin.site.register(Features)
+admin.site.register(Collections)
--- pages/views.py
+++ pages/views.py
@@ -3,7 +3,6 @@ from .models import Products
 
 
 # TODO: Add HTML template to render main page
-# TODO: Add function to randomly (for now) select products to showcase on the main page
 # TODO: Add search bar that renders page specified in the search
 def home(request):
     products = Products.objects.all()
