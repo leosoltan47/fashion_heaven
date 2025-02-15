20d8fd22101d29d96fd34e3a5f36e8c7f404c58a
Renter-0 <nadox45@gmail.com>
2025-02-15 16:19:23 +0200

    refactor: remove `FIXME`

--- pages/views.py
+++ pages/views.py
@@ -1,15 +1,19 @@
 # TODO: Add logger and profiler to monitor performance
+from django.db.models import Prefetch
 from django.shortcuts import render
-from .models import Products
+from .models import ProductDetails, Products
 
 
 # TODO: Add search bar that renders page specified in the search
 def home(request):
-    # Home page is designed to show 10 products
-    # FIXME: Severe performance hit due to this query
+    in_stock = ProductDetails.objects.filter(stock__gt=0)  # type: ignore
+    # Show 10 products that has at least one variant
+    # as information like color are stored in variants
     products = (
-        Products.objects.only("name", "price_in_dollars")
-        .prefetch_related("details")
+        Products.objects.filter(category__isnull=False)  # type: ignore
+        .prefetch_related(Prefetch("details", queryset=in_stock))
+        .distinct()
+        .filter(details__isnull=False)
         .order_by("id")[:10]
     )
     context = {"products": products}
