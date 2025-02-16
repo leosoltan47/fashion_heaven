c97744484b2ff17e2ed690e6f63723972ab7f7ad
Renter-0 <nadox45@gmail.com>
2025-02-16 18:30:29 +0200

    feat: update frontend
    Co-authored-by: leosoltan47 <leosoltan46@users.noreply.github.com>

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,15 @@
 module.exports = {
   plugins: {
+    'postcss-import': {},
+    'tailwindcss/nesting': {},
     tailwindcss: {},
     autoprefixer: {},
+    'postcss-preset-env': {
+      features: {
+        'nesting-rules': true,
+        'custom-properties': false,
+      },
+    },
+    ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {})
   },
 } 
