eefd4b026c059551238ae31e5d902531b27be29d
Renter-0 <nadox45@gmail.com>
2025-02-19 20:17:50 +0200

    fix!: enforce variant creation on product creation

--- pages/admin.py
+++ pages/admin.py
@@ -8,7 +8,8 @@ admin.site.register(Collections)
 
 @admin.register(ProductDetails)
 class ProductDetailsAdmin(admin.ModelAdmin):
-    list_display = ["id", "product__name", "color", "size", "stock"]
+    list_display = ["id", "color", "size", "stock"]
+
     # TODO: Add `color` to `list_filter`
     # TODO: Add custom `stock` filter to display greater or less than input
     list_filter = ["size", "stock"]
@@ -29,24 +30,5 @@ class ProductDetailsAdmin(admin.ModelAdmin):
 
 @admin.register(Products)
 class ProductsAdmin(admin.ModelAdmin):
-    list_display = ["name", "category", "displayed_price", "gender_and_age"]
-    list_filter = ["category", "gender_and_age", "currency"]
-
-    @admin.display(description="Price")
-    def displayed_price(self, obj):
-        return f"{obj.price} {obj.currency}"
-
-    def changelist_view(self, request, extra_context=None):
-        """
-        Display warning if there exists products without variants
-        """
-        products_with_variants = ProductDetails.objects.values_list(
-            "product_id", flat=True
-        )
-        products_without_variants = Products.objects.exclude(
-            pk__in=products_with_variants
-        )
-        if products_without_variants.exists():
-            message = mark_safe("There're are products that doest have any variants")
-            self.message_user(request, message, level="warning")
-        return super().changelist_view(request, extra_context)
+    list_display = ["name", "category", "price_in_dollars", "gender_and_age"]
+    list_filter = ["category", "gender_and_age"]
