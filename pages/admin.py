0d2c15744cdcdb491c27e610e217d1372c616ea6
Renter-0 <nadox45@gmail.com>
2025-03-04 19:20:02 +0200

    feat: add filter stock by range to admin panel

--- pages/admin.py
+++ pages/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django import forms
 from django.db import models
 from django.utils.safestring import mark_safe
+from django.utils.translation import gettext_lazy
 from .models import Categories, Collections, Features, ProductDetails, Products
 from currency_converter import CurrencyConverter
 
@@ -44,13 +45,78 @@ class ProductsAdminForm(forms.ModelForm):
         return cleaned_data
 
 
+class StockListFilter(admin.SimpleListFilter):
+    title = gettext_lazy("Stock ranges")
+    parameter_name = "stock"
+    template = "admin/stock_filter.html"
+    MAX_INT = 2147483647
+
+    def lookups(self, request, model_admin):  # type: ignore
+        return ((),)
+
+    @staticmethod
+    def _parse(query: str) -> int | tuple[int, int]:
+        """
+        Parse query string with '..' as separator to get the range. If separator
+        is not present the parsed number is returned. If starting point is no
+        specified '0' is assumed. If end point is not specified biggest integer
+        that `django.models.PositiveIntegerField` can store is returned.
+
+        Raises `ValueError` if string can't be parsed or if multiple
+        separators provided
+        """
+        query = query.strip()
+        if query == "":
+            raise ValueError("Query string is empty")
+
+        if len(temp := query.split(sep="..")) == 1:
+            if not temp[0].isdigit():
+                raise ValueError("Only digits and '..' separator are allowed")
+
+            return int(temp[0])
+        elif len(temp) == 2:
+            not_valid = lambda value: not value.isdigit() and value != ""
+            if not_valid(temp[0]) or not_valid(temp[1]):
+                raise ValueError("Only digits and '..' separator are allowed")
+
+            return int(temp[0] or 0), int(temp[1] or StockListFilter.MAX_INT)
+        else:
+            raise ValueError(
+                f"There could be only 1 '..' seperator. Found: {len(temp)}"
+            )
+
+    def queryset(self, request, queryset):
+        req = request.GET.get("stock") or ""
+        if req == "":
+            return queryset.order_by("id")
+        range_ = StockListFilter._parse(req)
+        if type(range_) == int:
+            kwargs = {"stock": range_}
+        elif type(range_) == tuple:
+            kwargs = {"stock__gte": range_[0], "stock__lt": range_[1]}
+        else:
+            return queryset.order_by("id")
+        return queryset.filter(**kwargs).order_by("id")
+
+    def choices(self, changelist):
+        # Grab only the "all" option.
+        all_choice = next(super().choices(changelist))  # type: ignore
+        all_choice["query_parts"] = (
+            (k, v)
+            for k, v in changelist.get_filters_params().items()
+            if k != self.parameter_name
+        )
+
+        yield all_choice
+
+
 @admin.register(ProductDetails)
 class ProductDetailsAdmin(admin.ModelAdmin):
+
     list_display = ["id", "color", "size", "stock"]
 
     # TODO: Add `color` to `list_filter`
-    # TODO: Add custom `stock` filter to display greater or less than input
-    list_filter = ["size", "stock"]
+    list_filter = ["size", StockListFilter]
 
     # TODO: Implement low stock threshold notification, alerting admins when a
     # product's inventory falls below a specified level, triggering a warning
@@ -59,6 +125,8 @@ class ProductDetailsAdmin(admin.ModelAdmin):
         """
         Display warning if there're out of stock variants
         """
+
+        # Warn if stock is empty
         out_of_stock_variants = ProductDetails.objects.filter(stock=0)
         if out_of_stock_variants.exists():
             message = mark_safe("There are variants that are out of stock")
