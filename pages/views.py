f872dd527c94db7eb90c412473e679824fb2c34e
Renter-0 <nadox45@gmail.com>
2025-04-24 15:39:43 +0300

    refactor: populate catalog page with less SQL queries

--- pages/views.py
+++ pages/views.py
@@ -83,23 +83,32 @@ def catalog(request, title):
         "B": "Boys",
         "G": "Girls",
     }
-    in_stock = ProductDetails.objects.filter(stock__gt=0)
+
+    # In order to reduce number of SQL queries prefetch `ProductImages` and `Color` tables
+    # as they're going to be used to populate context
+    in_stock = ProductDetails.objects.filter(stock__gt=0).prefetch_related(
+        "productimages_set", "productimages_set__color_set"
+    )
     objects = (
-        Products.objects.prefetch_related(Prefetch("details", queryset=in_stock))
+        Products.objects.select_related("category")
+        .prefetch_related(Prefetch("details", queryset=in_stock))
         .distinct()
         .filter(details__isnull=False)
+        # Get only the records corresponding to catalog
+        # Ex: catalog/men will show only outfits for Men, catalog/women for Women etc
         .filter(
             gender_and_age__in=gender_filters.get(title, ["W", "M", "U", "G", "B", "K"])
         )
         .order_by("id")
     )
+
     products = [
         {
             "id": obj.pk,
             "name": obj.name,
             "price": obj.price_in_dollars,
             "colors": [
-                [color.color_code for color in image.color_set.all()]
+                (*[color.color_code for color in image.color_set.all()],)
                 for variant in obj.details.all()
                 for image in variant.productimages_set.all()
             ],
