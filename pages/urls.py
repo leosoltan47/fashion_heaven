1cc044332642461b2ee714edc65f60f9751982f1
Renter-0 <nadox45@gmail.com>
2025-05-19 20:03:07 +0300

    feat: fill bag page with product variants
    - URLConf was modified to accept string representing variant ids
      separated via "&". Example: &1&2&29
    - Page is filled with existing variant ids that are in stock
    - If there's no products, payment menu won't show up

--- pages/urls.py
+++ pages/urls.py
@@ -10,9 +10,14 @@ urlpatterns = [
         r"wishlist/(?P<id_list>(?:ids\[\]=\d+&?)+)", views.wishlist, name="wishlist"
     ),
     path("bag/", views.bag, name="bag"),
+    path("bag/<str:ids>", views.bag, name="bag"),
     path("catalog/<str:title>", views.catalog, name="catalog"),
     path("products/<int:product_id>", views.product, name="product"),
-    path('product-detail/<int:product_id>/', views.product_detail_page, name='product_detail_page'),
-    path('support/', views.support_page, name='support'),
-    path('returns-exchanges/', views.returns_exchanges_view, name='returns_exchanges'),
+    path(
+        "product-detail/<int:product_id>/",
+        views.product_detail_page,
+        name="product_detail_page",
+    ),
+    path("support/", views.support_page, name="support"),
+    path("returns-exchanges/", views.returns_exchanges_view, name="returns_exchanges"),
 ]
