2bb22ed51f24cd656ac616d4c91de0989fbd4e92
Renter-0 <nadox45@gmail.com>
2025-05-31 21:58:55 +0300

    feat: implement register and log_in

--- original_name/views.py
+++ original_name/views.py
@@ -1,8 +1,40 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.contrib.auth import authenticate, login
+from django.http import HttpRequest, HttpResponseForbidden
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-class LoginView(TemplateView):
-    template_name = 'original_name/login.html'
 
-class RegisterView(TemplateView):
-    template_name = 'original_name/register.html' 
+def log_in(request: HttpRequest):
+    if request.method == "GET":
+        return render(request, "original_name/login.html")
+    # This view only support GET and POST
+    if request.method != "POST":
+        return HttpResponseForbidden()
+
+    email = request.POST["email"]
+    password = request.POST["password"]
+
+    user: User | None = authenticate(
+        request, email=email, username=email, password=password
+    )
+    if not user:
+        return HttpResponseForbidden()
+
+    login(request, user)
+    return redirect(reverse("pages:home"), permanent=True, preserve_request=True)
+
+
+def register(request: HttpRequest):
+    if request.method == "GET":
+        return render(request, "original_name/register.html")
+    if request.method != "POST":
+        return HttpResponseForbidden()
+
+    email = request.POST["email"]
+    password = request.POST["password"]
+
+    user = User.objects.create_user(username=email, email=email, password=password)
+    user.save()
+    login(request, user)
+    return redirect(reverse("pages:home"), permanent=True, preserver_request=True)
