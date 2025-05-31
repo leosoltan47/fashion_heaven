89888da0115ad3087dbe976b8f0fad4ade61bbab
Renter-0 <nadox45@gmail.com>
2025-05-31 21:58:55 +0300

    feat: implement register and log_in

--- original_name/urls.py
+++ original_name/urls.py
@@ -3,14 +3,14 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from debug_toolbar.toolbar import debug_toolbar_urls
-from .views import LoginView, RegisterView
+from .views import log_in, register
 
 urlpatterns = (
     [
         path("admin/", admin.site.urls),
         path("", include("pages.urls")),
-        path('login/', LoginView.as_view(), name='login'),
-        path('register/', RegisterView.as_view(), name='register'),
+        path("login/", log_in, name="login"),
+        path("register/", register, name="register"),
     ]
     + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     + debug_toolbar_urls()
